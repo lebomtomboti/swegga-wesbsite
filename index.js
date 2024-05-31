3672211744757bd89967c0a55361b7250c306120
lebomtomboti <lebomtomboti17@gmail.com>
2024-05-31 13:35:42 +0200

    more text

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function calculateTotal(){
     let femalePrice = 150.95;
     let malePrice = 180.95;
-    let femaleQuantity = parseInt
+    let femaleQuantity = parseInt(document.getElementById('femaleQuantity').value);
+    let maleQuantity = parseInt(document.getElementById('maleQuantity').value);
+    let femaleTotal = femalePrice * femaleQuantity;
+    let maleTotal = malePrice * maleQuantity;
+    let orderTotal = femaleTotal + maleTotal;
+
+    document.getElementById('femaleTotal').innerHTML = '$' + femaleTotal;
+    document.getElementById('male')
 }
